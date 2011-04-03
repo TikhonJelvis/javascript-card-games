d8233b34c7287ae128a129ca539e0c0496ac7d92
jonathan.ewart@gmail.com <jonathan.ewart@gmail.com>
2011-04-03 08:36:01 +0000

    added draggable stuff to Board

--- js/test.js
+++ js/test.js
@@ -1,11 +1,11 @@
 function runTest() {
-	var deck=new Deck();
+	var deck=new Deck(defaultType);
 	deck.setX(1);
 	deck.setY(1);
 	gameBoard.addDeck(deck);
 	deck.addTop(new Card(5,"s"));
 	deck.addTop(new Card(6,"s"));
-	var deck2=new Deck();
+	var deck2=new Deck(defaultType);
 	deck2.setX(2);
 	deck2.setY(1);
 	gameBoard.addDeck(deck2);
@@ -21,20 +21,9 @@ function runTest() {
 	deck2.addTop(new Card(1,"h"));
 	deck2.addTop(new Card(2,"c"));
 	deck2.addTop(new Card(3,"s"));
-	var deck3=new Deck();
+	var deck3=new Deck(collapsedType);
 	deck3.setX(2);
 	deck3.setY(2);
 	gameBoard.addDeck(deck3);
-	deck3.addTop(new Card(1,"h"));
-	deck3.addTop(new Card(2,"c"));
-	deck3.addTop(new Card(3,"s"));
-	deck3.addTop(new Card(1,"h"));
-	deck3.addTop(new Card(2,"c"));
-	deck3.addTop(new Card(3,"s"));
-	deck3.addTop(new Card(1,"h"));
-	deck3.addTop(new Card(2,"c"));
-	deck3.addTop(new Card(3,"s"));
-	deck3.addTop(new Card(1,"h"));
-	deck3.addTop(new Card(2,"c"));
-	deck3.addTop(new Card(3,"s"));
+	deck3.addTop(new Card(9,"h"));
 }
