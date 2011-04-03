423eb3e8e69b4fdf5d6faea3187e0e22c4e2a56e
tikhonjelvis <tikhonjelvis@d69fd2e8-333d-9df6-07c3-69fce3c107c8>
2011-04-03 09:41:53 +0000

    Added a filter to solitaire and updated how it interfaces with Board

--- trunk/js/Solitaire.js
+++ trunk/js/Solitaire.js
@@ -3,30 +3,39 @@
  */
 function Solitaire() {
     var board = new Board({
-		rootId: "Klondike"
-	});
+        rootId : "Klondike",
+        magicalX : 106
+    });
 
-    var totalDeck = new Deck(collapsedType, 0, 0);
+    var totalDeck = new Deck(board.collapsedType, 0, 0);
     totalDeck.initialize();
     totalDeck.shuffle();
     board.addDeck(totalDeck);
 
     var pile;
-    var discard = new Deck(collapsedType, 1, 0);
+    var discard = new Deck(board.collapsedType, 1, 0);
     board.addDeck(discard);
     var endPiles = [];
     
     for (var i = 1; i <= 7; i++) {
-        pile = new Deck(defaultType, i - 1, 1);
+        pile = new Deck(board.defaultType, i - 1, 1);
         totalDeck.deal(pile, i, true);
-        pile.peek().setFaceUp(true);
+
+        var topCard = pile.peek();
+        topCard.setFaceUp(true);
+        pile.setFilter(function (card) {
+            if (topCard) {
+                return (card.getColor() != topCard.getColor()) &&
+                    (topCard.getSuit() - card.getSuit()) == 1;
+            } else {
+                return card.getRank() == 13;
+            }
+        });
         board.addDeck(pile);
     }
     
     for (i = 0; i < 4; i++) {
-        endPiles[i] = new Deck(collapsedType, 4 + i, 0);
+        endPiles[i] = new Deck(board.collapsedType, 3 + i, 0);
         board.addDeck(endPiles[i]);
     }
-
-    console.log(board);
 }
