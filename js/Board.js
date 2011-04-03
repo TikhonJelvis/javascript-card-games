d0b461af2921af85447a512c1c49f2b5dc772e9e
jonathan.ewart@gmail.com <jonathan.ewart@gmail.com>
2011-04-03 05:03:37 +0000

    updated Board.js to fix bugs and added test.js

--- js/Board.js
+++ js/Board.js
@@ -4,55 +4,65 @@ var __EXTENSION_TYPE = "gif";
 var __IMAGES_DIRECTORY = "images/";
 var __CARD_WIDTH = 50;
 var __CARD_HEIGHT = 50;
-var __DECK_HEIGHT = 200;
+var __DECK_HEIGHT = 100;
+var __MAGICAL_UNIT_X = 150;
+var __MAGICAL_UNIT_Y = 150;
+var __HAND_WIDTH = 500;
 function Board(game) {
 
 	//PRIVATE INSTANCE VARIABLES
-	game=game||new Game();
+	//game=game||new Game();
 	var root=$(__MY_ROOT);
+	root.addClass("board");
 	var decks = new Array();
 	
 	//PRIVATE FUNCTIONS
 	
 	function drawDeck(deck) {
-		return reDrawDeck([deck, $("<div>")]);
+		var div = $("<div>");
+		div.addClass("deck");
+		root.append(div);
+		return reDrawDeck([deck, div]);
 	}
 	
 	function reDrawDeck(deckArr) {
 		var deck = deckArr[0];
 		var div = deckArr[1];
 		var isHand = isNaN(deck.getX());
-		var offsetX = isHand ? __CARD_WIDTH : 0;
-		
-		for(int i = 0; i < deck.getSize(); i++) {
+		//the offset for hands is proportional to the size of the hand
+		var offsetX = isHand ? ((__HAND_WIDTH - __CARD_WIDTH) / deck.getSize()) : 0;
+		//the offset for decks is proportional to the size of the deck
+		var offsetY = isHand ? 0 : ((__DECK_HEIGHT - __CARD_HEIGHT) / deck.getSize());
+		var cards = deck.getCards();
+		for(var i = 0; i < cards.length; i++) {
 			var holder = $("<div>");
-			holder.setAttribute("class", "cardInDeck");
-			holder.style.setProperty("top", offset * i);
+			holder.addClass("card");
+			holder.css("top", (offsetY * i)+"px");
+			holder.css("left", (offsetX * i)+"px");
 			var img = $("<img>");
-			var card=deck[i];
-			img.setAttribute("src", card.isFaceUp() ? (__IMAGES_DIRECTORY+card.getRankAsString()+card.getSuit()+__EXTENSION_TYPE) : (__IMAGES_DIRECTORY+"back."+__EXTENSION_TYPE));
-			img.style.setProperty("opacity", __DECK_OPACITY);
-			holder.appendChilde(img);
-			div.appendChild(holder);
+			var card = cards[i];
+			img.attr("src", card.isFaceUp() ? (__IMAGES_DIRECTORY+card.getRank()+card.getSuit()+"."+__EXTENSION_TYPE) : (__IMAGES_DIRECTORY+"back."+__EXTENSION_TYPE));
+			img.css("opacity", __DECK_OPACITY);
+			holder.append(img);
+			div.append(holder);
 		}
 		return div;
 	}
 	
 	//PUBLIC FUNCTIONS
 	
-	this.addPile = function(pile) {
-		var i=piles.push([pile,drawDeck(pile)]);
-		return i;
+	this.addDeck = function (deck) {
+		return decks.push([deck, drawDeck(deck)]);
 	};
 	
-	this.addHand = function(hand) {
-		var i=hands.push([hand,drawHand(hand)]);
-		return i;
+	this.update = function (deck) {
+		for(var i = 0; i < decks.length; i++) {
+			if(deck == decks[i][0]) {
+				return reDrawDeck(decks[i]);
+			}
+		}
 	};
-	
-	this.addDeck = function(deck) {
-		var i=decks.push([deck,drawDeck(deck)]);
-		return i;
-	};
-	
 }
+$(document).ready(function() {
+	gameBoard = new Board();
+});
