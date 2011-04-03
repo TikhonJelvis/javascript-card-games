921fd7ac03d8a50549bdf248f099fd12455d514a
jonathan.ewart@gmail.com <jonathan.ewart@gmail.com>
2011-04-03 03:09:38 +0000

    Added Card.js and Hand.js classes -- Jonathan Ewart

---
+++ js/Card.js
@@ -0,0 +1,70 @@
+/**
+* This class represents a card.
+* A card has an immutable rank and suit as well as a variable relative positon (x,y)
+*/
+function Card(rank, suit) {
+
+	//PRIVATE INSTANCE VARIABLES
+	
+	//(x,y) relative position of this card
+	var relativePosition=[0,0];
+	
+	
+	
+	//PUBLIC FUNCTIONS
+	
+	//suit getter method
+	//returns String suit
+	this.getSuit = function () {
+		return suit;
+	};
+	
+	//rank getter method
+	//returns int rank
+	this.getRank = function () {
+		return rank;
+	};
+	
+	//rank getter method
+	//returns String rank
+	this.getRankAsString=function() {
+		switch(rank) {
+			case 11:
+				return "J";
+			case 12:
+				return "Q";
+			case 13:
+				return "K";
+			case 14:
+				return "A";
+			default:
+				return rank;
+		}
+	};
+	
+	//relative x position getter method
+	//returns int xPos
+	this.getX = function () {
+		return relativePosition[0];
+	};
+	
+	//relative y position getter method
+	//returns int yPos
+	this.getY = function () {
+		return relativePosition[1];
+	};
+	
+	//relative x position setter method
+	//param int xPos
+	//return int xPos
+	this.setX = function (x) {
+		return relativePosition[0]=x;
+	};
+	
+	//relative y position setter method
+	//param int yPos
+	//return int yPos
+	this.setY = function (y) {
+		return relativePosition[1]=y;
+	};
+}
---
+++ js/Hand.js
@@ -0,0 +1,46 @@
+/**
+* Hand is a class for holding an array of cards that represents a users hand
+* It contains adder, getter, remover methods for the private array which represents the cards
+*/
+function Hand() {
+	
+	//PRIVATE INSTANCE VARIABLES
+	
+	//cards is represented as an array of cards
+	var myCards=new Array();
+	
+	
+	//PUBLIC FUNCTIONS
+	
+	//adder method for single cards
+	//param Card card is the card to add
+	//return int currentLength, the current number of cards in the hand
+	this.addCard=function (card) {
+		return myCards.push(card);
+	};
+	
+	//adder method for an array of cards
+	//parm Card[] cards is an array of cards to add
+	//returns the current contents of the hand as an array
+	this.addCards=function (cards) {
+		return myCards=myCards.concat(cards);
+	};
+	
+	//getter method for the cards
+	//returns the current contents of the hand as an array
+	this.getCards=function() {
+		return cards;
+	};
+	
+	//remover method for the cards
+	//param Card card is the card to remove
+	//returns the removed card or null if the card was not found
+	this.removeCard=function(card) {
+		for(var i=0;i<myCards.length;i++) {
+			if(myCards[i]==card) {
+				return myCards.splice(i,1)[0];
+			}
+		}
+		return null;
+	};
+}
