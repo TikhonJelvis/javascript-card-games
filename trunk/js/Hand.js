5d4d4c7f1cc703aaee1a4a0c621a021c4790a5a0
jonathan.ewart@gmail.com <jonathan.ewart@gmail.com@d69fd2e8-333d-9df6-07c3-69fce3c107c8>
2011-04-03 03:17:13 +0000

    Formatted stuff in Card and Hand

--- trunk/js/Hand.js
+++ trunk/js/Hand.js
@@ -6,38 +6,48 @@ function Hand() {
 	
 	//PRIVATE INSTANCE VARIABLES
 	
-	//cards is represented as an array of cards
-	var myCards=new Array();
+	/**
+	* cards is represented as an array of cards
+	*/
+	var myCards = new Array();
 	
 	
 	//PUBLIC FUNCTIONS
 	
-	//adder method for single cards
-	//param Card card is the card to add
-	//return int currentLength, the current number of cards in the hand
-	this.addCard=function (card) {
+	/**
+	* adder method for single cards
+	* param Card card is the card to add
+	* return int currentLength, the current number of cards in the hand
+	*/
+	this.addCard = function (card) {
 		return myCards.push(card);
 	};
 	
-	//adder method for an array of cards
-	//parm Card[] cards is an array of cards to add
-	//returns the current contents of the hand as an array
-	this.addCards=function (cards) {
-		return myCards=myCards.concat(cards);
+	/**
+	* adder method for an array of cards
+	* parm Card[] cards is an array of cards to add
+	* returns the current contents of the hand as an array
+	*/
+	this.addCards = function (cards) {
+		return myCards = myCards.concat(cards);
 	};
 	
-	//getter method for the cards
-	//returns the current contents of the hand as an array
-	this.getCards=function() {
+	/**
+	* getter method for the cards
+	* returns the current contents of the hand as an array
+	*/
+	this.getCards = function () {
 		return cards;
 	};
 	
-	//remover method for the cards
-	//param Card card is the card to remove
-	//returns the removed card or null if the card was not found
-	this.removeCard=function(card) {
-		for(var i=0;i<myCards.length;i++) {
-			if(myCards[i]==card) {
+	/**
+	* remover method for the cards
+	* param Card card is the card to remove
+	* returns the removed card or null if the card was not found
+	*/
+	this.removeCard = function (card) {
+		for(var i=0; i < myCards.length; i++) {
+			if(myCards[i] == card) {
 				return myCards.splice(i,1)[0];
 			}
 		}
