bec1790519beedae1d75b3beabb985b4b172f102
tikhonjelvis <tikhonjelvis@d69fd2e8-333d-9df6-07c3-69fce3c107c8>
2011-04-03 08:54:33 +0000

    Fixed the dealing in Solitaire.js by adding all of the decks to the board.

--- trunk/js/Solitaire.js
+++ trunk/js/Solitaire.js
@@ -2,21 +2,27 @@
  * A game of Klondike Solitaire.
  */
 function Solitaire() {
-    var board = new Board();
+    var board = new Board("Klondike");
+
     var totalDeck = new Deck(collapseType, 0, 0);
     totalDeck.initialize();
     totalDeck.shuffle();
+    board.addDeck(totalDeck);
+
     var pile;
     var discard = new Deck(collapseType, 1, 0);
+    board.addDeck(discard);
     var endPiles = [];
     
     for (var i = 1; i <= 7; i++) {
         pile = new Deck(defaultType, i - 1, 1);
         totalDeck.deal(pile, i, true);
         totalDeck.peek().setFaceUp(true);
+        board.addDeck(pile);
     }
     
     for (i = 0; i < 4; i++) {
         endPiles[i] = new Deck(collapseType, 4 + i, 0);
+        board.addDeck(endPiles[i]);
     }
 }
